906dbe6db0c0b80080327ced4f700a89e7c50af3
Random <78389553+Sam772@users.noreply.github.com>
2023-02-18 14:01:55 +0000

    Player Sprite & Controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float PlayerSpeed;
+    private Rigidbody2D _rigidBody;
+    private Vector2 _playerDirection;
+
+    void Start() {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float directionY = Input.GetAxis("Vertical");
+        _playerDirection = new Vector2(0, directionY).normalized;
+    }
+
+    void FixedUpdate() {
+        _rigidBody.velocity = new Vector2(0, _playerDirection.y * PlayerSpeed);
+    }
+}
