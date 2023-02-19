f45665c0af923821f8f6e987a69e90f660490ee7
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-02-19 09:57:15 +0000

    added level text

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -21,7 +21,30 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(target.Equals(other.name)){
-            transform.parent.GetComponent<Player>().UpdateScore(5);
+            Player player = transform.parent.GetComponent<Player>();
+            player.UpdateScore(5);
+            switch(player.GetScore()){
+            case 10:
+                player.IncreaseLevel();
+                GameManager.instance.updateTextLevel(player.GetLevel());
+                break;
+            case 20:
+                player.IncreaseLevel();
+                GameManager.instance.updateTextLevel(player.GetLevel());
+                break;
+            case 30:
+                player.IncreaseLevel();
+                GameManager.instance.updateTextLevel(player.GetLevel());
+                break;
+            case 40:
+                player.IncreaseLevel();
+                GameManager.instance.updateTextLevel(player.GetLevel());
+                break;
+            case 50:
+                player.IncreaseLevel();
+                GameManager.instance.updateTextLevel(player.GetLevel());
+                break;
+        }
             Instantiate(explosion, transform.position, transform.rotation);
             Destroy(this.gameObject);
         }
